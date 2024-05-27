bafece2caba6ed63ab6517dc0076b6251f123a54
Lorenzo Fravolini <lorenzofravolini@MBP-di-Lorenzo.homenet.telecomitalia.it>
2024-05-27 20:06:53 +0200

    finished

---
+++ Weather app/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  Weather app
+//
+//  Created by Lorenzo Fravolini on 18/12/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
